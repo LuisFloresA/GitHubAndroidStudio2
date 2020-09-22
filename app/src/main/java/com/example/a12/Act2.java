aca4cf869057674e24d7d53b3867482d81f11e26
Luis Flores <luisp.floresa@gmail.com>
2020-09-21 21:45:39 -0300

    Initial commit aaaa

---
+++ app/src/main/java/com/example/a12/Act2.java
@@ -0,0 +1,21 @@
+package com.example.a12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Act2 extends AppCompatActivity  {
+    TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act2);
+
+        tv1 = findViewById(R.id.txtMostrar);
+
+        String valor = getIntent().getStringExtra("dato");
+        tv1.setText(valor);
+    }
+
+}
---
+++ app/src/main/java/com/example/a12/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.a12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnAct2;
+    EditText txt1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAct2 = findViewById(R.id.nextAct);
+        txt1 = findViewById(R.id.input);
+
+        btnAct2.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.nextAct:
+                Toast.makeText(this, "Paso", Toast.LENGTH_LONG).show();
+                Intent sig = new Intent(this,Act2.class);
+                sig.putExtra("dato",txt1.getText().toString());
+                startActivity(sig);
+                break;
+        }
+    }
+}
