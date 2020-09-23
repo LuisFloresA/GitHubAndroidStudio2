e688781b77ab738ce508773ce102d1873e8d8415
Luis Flores <luisp.floresa@gmail.com>
2020-09-23 10:01:49 -0300

    v1.2

--- app/src/main/java/com/example/a12/MainActivity.java
+++ app/src/main/java/com/example/a12/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.nextAct:
-                Toast.makeText(this, "paaaa", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Toast Test", Toast.LENGTH_LONG).show();
                 Intent sig = new Intent(this,Act2.class);
                 //sig.putExtra("dato",txt1.getText().toString());
                 //startActivity(sig);
